009f7c1d63eb44344a83021332580ad0c17aa9b5
KingsleyZ <kingsleyzheng0716@gmail.com>
2014-11-21 14:47:56 -0800

    Initial Commit

---
+++ DA/Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Objects;
+using MySql.Data.MySqlClient;
+
+namespace DA
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; } 
+
+        public Context()
+            : base(new MySqlConnection("Server=localhost;Database=greenreceipt;Uid=root;Pwd=Aa8175014;"), true)
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>();
+        }
+    }
+}
---
+++ GreenReceiptApi/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DA;
+using Domain.Objects;
+
+namespace GreenReceiptApi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        // GET api/users
+        public IEnumerable<User> Get()
+        {
+            List<User> users;
+            using (Context context = new Context())
+            {
+                users = context.Users.ToList();
+            }
+            return users;
+        }
+
+        // GET api/users/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/users
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/users/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/users/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
