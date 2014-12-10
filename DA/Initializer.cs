55cae29766e9c3994afee3971269f263d0c6bd96
KingsleyZ <kingsleyzheng0716@gmail.com>
2014-12-10 11:57:20 -0700

    Added Context Initializer for seeding data. Added contexts label in web.config in order to user Initializer.

--- DA/Initializer.cs
+++ DA/Initializer.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using Domain.Objects;
+using Domain;
 
 namespace DA
 {
@@ -23,6 +25,17 @@ namespace DA
         protected override void Seed(Context context)
         {
             //TODO: seed data
+            var receipts = new List<Receipt>
+            {
+                new Receipt {StoreId = 1, Barcode="123", PurchaseDate = new DateTime(), Tax = 1.0M, SubTotal = 1.5M, Total = 2.5M, CardType = CardType.Visa, LastFourCardNumber = "1000", ReturnDate = new DateTime(15), 
+                  CashierId = "1", MiscMessage = "nil", User = new User(), Store = new Store(), ReceiptItems = new List<ReceiptItem>},
+            };
+
+            var users = new List<User>
+            {
+
+            };
+ 
         }
     }
 #endif
